c2a528d69e16b8bb0035fb202372a54499e94e2f
jesse keane <me@jessekeane.me>
2013-10-27 13:04:20 -0700

    add node

--- run.sh
+++ run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+sudo add-apt-repository ppa:chris-lea/node.js
 sudo apt-get update && sudo apt-get upgrade
 sudo apt-get install fail2ban
 sudo apt-get install unattended-upgrades
-
+sudo apt-get install nodejs
